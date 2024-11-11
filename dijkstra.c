f1650b7a6ccbc885cdc91490349fb707e79d2475
SayyedTaheraEhsan <s.tahera123@gmail.com>
2024-11-11 18:07:07 +0530

    Update dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -4,7 +4,7 @@
 
 #define V 5 // Number of vertices in the graph
 
-// Function to find the vertex with the minimum distance value
+
 int minDistance(int dist[], int sptSet[]) {
     int min = INT_MAX, min_index;
     for (int v = 0; v < V; v++) {
