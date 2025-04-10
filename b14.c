2cb2ddbbb7392ce747a337f72063914dd91796d8
SayyedTaheraEhsan <s.tahera123@gmail.com>
2025-04-10 20:22:03 +0530

    Create b14.c

---
+++ b14.c
@@ -0,0 +1,12 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+// Dummy user data (In a real app, this would come from a database)
