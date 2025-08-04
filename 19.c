39f000dc1b390bae773813c80f83588cdc829e51
SayyedTaheraEhsan <s.tahera123@gmail.com>
2025-08-03 22:38:25 -1200

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,19 @@
+//3.​ Read three integers and find the largest among them.
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three integers:\n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c) {
+        printf("%d is the largest number.\n", a);
+    } else if (b >= a && b >= c) {
+        printf("%d is the largest number.\n", b);
+    } else {
+        printf("%d is the largest number.\n", c);
+    }
+
+    return 0;
+}
