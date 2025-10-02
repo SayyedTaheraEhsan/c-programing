393431d0a8db49fa4e95016c6e78ae77c05633de
SayyedTaheraEhsan <s.tahera123@gmail.com>
2025-10-02 23:54:37 +0530

    Update MS-2302-assgn1.c

--- MS-2302-assgn1.c
+++ MS-2302-assgn1.c
@@ -11,34 +11,27 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);               // Initialize MPI
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // Get process rank
     MPI_Comm_size(MPI_COMM_WORLD, &size);  // Get total processes
-
     if (rank == 0) {
         // Take input from user
         printf("Enter total number of elements (N): ");
         scanf("%d", &N);
-
-        // Allocate and read numbers
+        // Allocate and read number
         numbers = (int *)malloc(N * sizeof(int));
         printf("Enter %d numbers:\n", N);
         for (int i = 0; i < N; i++) {
             scanf("%d", &numbers[i]);
         }
     }
-
     // Broadcast N to all processors
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
     // Each process gets local_n elements
     local_n = N / size;
     int remainder = N % size;
-
     // Handle uneven distribution
     if (rank < remainder) {
         local_n++;
     }
-
     sub_array = (int *)malloc(local_n * sizeof(int));
-
     // Scatter data to all processors
     // First, prepare counts and displacements on root
     int *sendcounts = NULL;
@@ -47,7 +40,6 @@ int main(int argc, char *argv[]) {
     if (rank == 0) {
         sendcounts = (int *)malloc(size * sizeof(int));
         displs = (int *)malloc(size * sizeof(int));
-
         int offset = 0;
         for (int i = 0; i < size; i++) {
             sendcounts[i] = N / size;
@@ -59,7 +51,6 @@ int main(int argc, char *argv[]) {
 
     MPI_Scatterv(numbers, sendcounts, displs, MPI_INT,
                  sub_array, local_n, MPI_INT, 0, MPI_COMM_WORLD);
-
     // Each process computes its local sum
     for (int i = 0; i < local_n; i++) {
         local_sum += sub_array[i];
